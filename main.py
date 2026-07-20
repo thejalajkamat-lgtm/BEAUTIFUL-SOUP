e4b0ceab563fca9c50dd59eff09c82215090d18c
Jalaj Kamat <thejalajkamat@gmail.com>
2026-07-20 22:37:47 +0530

    fuck

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import os
-import requests
-def fetchAndSaveToFile(url, path):
-    r = requests.get(url)
-    with open(path, "w") as f:
-        f.write(r.text)
-url = "https://economictimes.indiatimes.com/"
-fetchAndSaveToFile(url, "data/times.html")
-# r = requests.get(url)
-# print(r.text)
-
-# with open("demo.txt", "r") as f:
-#     data = f.read()
-#     print(data)
+from bs4 import BeautifulSoup
+with open('main.html', 'r') as file:
+    content = file.read()
+    print(content)
