fa83c69914fbe26dc3bc21c922940650e0b57e31
Jalaj Kamat <thejalajkamat@gmail.com>
2026-07-21 19:54:00 +0530

    revision

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from bs4 import BeautifulSoup
 with open('main.html', 'r') as file:
     content = file.read()
-    print(content)
+    # print(content)
+    soup = BeautifulSoup(content, 'lxml')
+    tags = soup.find_all('ul')
+    print(tags)
+    for fruits in tags:
+        print(fruits.text)
+    # print(soup.prettify())
